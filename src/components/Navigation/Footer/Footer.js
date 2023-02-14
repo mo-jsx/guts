57e5195aebbb90d40a5003a54712b27b600fe653
mojsx <mojsx.dev@gmail.com>
2023-02-14 18:56:58 +0100

    feat: add Footer component.

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -4,22 +4,23 @@ import "./footer.css";
 
 const Footer = ({ sections, ...rest }) => {
     return (
-        <div {...rest}>
+        <footer className="main_footer" {...rest}>
             {sections.map((section, index) => (
-                <div key={index}>
+                <div className="footer_section" key={index}>
                     <h2>{section.name}</h2>
-                    <ul>
+                    <ul className="section_titles">
                         {section.titles.map((title, index) => 
                             <SimpleLink
                                 content={title}
                                 href={`/${title}`}
                                 key={index}
+                                className="title"
                             />
                         )}
                     </ul>
                 </div>
             ))}
-        </div>
+        </footer>
     );
 };
 
