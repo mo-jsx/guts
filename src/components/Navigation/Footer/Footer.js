85ee27c46ddfdeb156b6973af296822f7b2311d3
mojsx <mojsx.dev@gmail.com>
2023-02-18 09:58:10 +0100

    styles: import styles from global style file, delete duplicate.

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -4,11 +4,11 @@ import "./footer.css";
 
 const Footer = ({ sections, ...rest }) => {
     return (
-        <footer className="main_footer" {...rest}>
+        <footer className="main_footer flex_row align_center" {...rest}>
             {sections.map((section, index) => (
-                <div className="footer_section" key={index}>
+                <div className="footer_section flex_col justify_sp_bet" key={index}>
                     <h2>{section.name}</h2>
-                    <ul className="section_titles">
+                    <ul className="section_titles flex_col justify_sp_bet">
                         {section.titles.map((title, index) => 
                             <SimpleLink
                                 content={title}
